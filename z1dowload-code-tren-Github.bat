379c76103585375fa01e63ba18f7fa1d17bf0b0f
adwewecat <89115538+adwewecat@users.noreply.github.com>
2021-08-30 09:30:28 +0700

    'note'

---
+++ z1dowload-code-tren-Github.bat
@@ -0,0 +1,4 @@
+CD /e e:\project-shoppe\shoppe
+git pull origin master
+Echo Chao Tam Biet
+PAUSE
---
+++ zupload-project-len-Github.bat
@@ -0,0 +1,13 @@
+CD /e e:\project-shoppe\shoppe
+
+git status
+
+git add -A
+
+GIT commit -m Nguyen-Anh-Dung
+
+GIT push origin master
+
+Echo Chao Tam Biet Nhe
+
+PAUSE
